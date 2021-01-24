b193bc383e133befd76cda0714d3e75122ea97f1
Blake Davis <Blakedav2088@gmail.com>
2021-01-24 03:52:31 -0500

    changed the title and a couple other items in the blog config file

--- blog.config.js
+++ blog.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   url: "https://nextjs-mdx-blog-starter.vercel.app/",
-  title: "Next.js MDX Blog Starter",
+  title: "<backendBlake/>",
   subtitle: "Content authoring with MDX. Powered by Next.js.", // longDescription : appears in landing page meta description
   description:
-    "Next.js MDX Blog Starter for building blogs with Next.js and MDX, including Theme UI Component Design System, Vercel Deployment and more.",
-  author: "John Polacek",
+    "This is a blog where I will share my  journey into tech and all that goes along with it! Always looking to learn and get better.",
+  author: "Blake Davis",
   analytics: "", // put your google analytics code here to add tracking
   shareImage: "https://nextjs-mdx-blog-starter.vercel.app/img/screenshot.png",
   shareImageAlt: "Next.js MDX Blog Starter Screenshot",
